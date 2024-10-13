95f9a8b8aa94091e962be9d93c9089b19892f9bc
zeemaarCA <zeemaarca@gmail.com>
2024-10-13 17:30:15 +0500

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,124 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import logo from "../../public/logo.png";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { HiMenuAlt3 } from "react-icons/hi";
+import Link from "next/link";
+
+const menuLinks = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "Services",
+    href: "/services",
+    subLinks: [
+      { name: "Web Development", slug: "web-development" },
+      { name: "Mobile Development", slug: "mobile-development" },
+      { name: "SEO Services", slug: "seo-services" },
+      { name: "Consulting", slug: "consulting" },
+    ],
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+  },
+  {
+    name: "Blog",
+    href: "/blog",
+  },
+];
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="bg-white py-2 border-b border-slate-200">
+        <nav className="container flex items-center justify-between">
+          <div className="logo">
+            <Image src={logo} alt="logo" width={130} height={100} />
+          </div>
+          <div className="nav-center hidden lg:block">
+            <ul className="flex items-center justify-start gap-6">
+              {menuLinks.map((link) => (
+                <li key={link.name} className="font-semibold">
+                  {link.subLinks ? (
+                    <DropdownMenu>
+                      <DropdownMenuTrigger className="focus-visible:outline-none">
+                        <span className="cursor-pointer flex items-center gap-1">
+                          {link.name} <MdKeyboardArrowDown />
+                        </span>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent className="mt-3">
+                        {link.subLinks.map((subLink) => (
+                          <DropdownMenuItem key={subLink.slug}>
+                            <Link href={`/${link.name.toLowerCase()}/${subLink.slug}`}>
+                              {subLink.name}
+                            </Link>
+                          </DropdownMenuItem>
+                        ))}
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  ) : (
+                    <Link href={link.href} className="">{link.name}</Link>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="nav-right flex items-center gap-2">
+            <Button variant="outline" className="btn-theme-outline">Sign In</Button>
+            <div className="menu-btn lg:hidden">
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Button><HiMenuAlt3 className="w-6 h-6" /></Button>
+                </SheetTrigger>
+                <SheetContent>
+                  <SheetHeader>
+                    <SheetTitle>Menu</SheetTitle>
+                  </SheetHeader>
+                  <div className="py-4">
+                    <ul className="flex flex-col items-start justify-start divide-y-2 w-full">
+                      {menuLinks.map((link) => (
+                        <li key={link.name} className="w-full py-3 font-semibold">
+                          {link.subLinks ? (
+                            <DropdownMenu>
+                              <DropdownMenuTrigger className="focus-visible:outline-none">
+                                <span className="cursor-pointer flex items-center gap-1">
+                                  {link.name} <MdKeyboardArrowDown />
+                                </span>
+                              </DropdownMenuTrigger>
+                              <DropdownMenuContent className="mt-3 w-64">
+                                {link.subLinks.map((subLink) => (
+                                  <DropdownMenuItem key={subLink.slug}>
+                                    <Link href={`/${link.name.toLowerCase()}/${subLink.slug}`}>
+                                      {subLink.name}
+                                    </Link>
+                                  </DropdownMenuItem>
+                                ))}
+                              </DropdownMenuContent>
+                            </DropdownMenu>
+                          ) : (
+                            <Link href={link.href}>{link.name}</Link>
+                          )}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                  <SheetFooter>
+                  </SheetFooter>
+                </SheetContent>
+              </Sheet>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,90 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+		extend: {
+			container: {
+				center: true,
+				padding: '1.25rem',
+			},
+			fontFamily: {
+  			montserrat: ["Montserrat", "sans-serif"],
+  			poppins: ["Poppins", 'sans-serif'],
+  			raleway: ["Raleway", 'sans-serif'],
+  			lora: ["Lora", 'serif']
+  		},
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+				},
+				custom: {
+          DEFAULT: '#9d4edd', // Custom primary button color
+          foreground: '#FFFFFF', // Text color for primary button
+          secondary: '#edf2f4', // Custom secondary button color
+          secondaryForeground: '#000000', // Text color for secondary button
+          danger: '#d80032', // Custom danger button color
+          dangerForeground: '#FFFFFF', // Text color for danger button
+        },
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+			},
+			keyframes: {
+				"shine": {
+					from: { backgroundPosition: '200% 0' },
+					to: { backgroundPosition: '-200% 0' },
+				},
+			},
+			animation: {
+				"shine": "shine 8s ease-in-out infinite",
+			},
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
