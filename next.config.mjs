d0608dccf68d83e04d6c5837945c620ae92e6ff1
zeemaarCA <zeemaarca@gmail.com>
2024-10-17 18:31:20 +0500

    add nextjs-monorepo-workaround-plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: ['@prisma/client'],
