4c91f58e128fd0896ce001efbf6a386de0c48c78
zeemaarCA <zeemaarca@gmail.com>
2024-10-17 18:16:25 +0500

    change package.json and config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  transpilePackages: ['@prisma/client'],
   images: {
     domains: ['lh3.googleusercontent.com', 'source.unsplash.com', 'fakestoreapi.com', 'images.pexels.com', 'firebasestorage.googleapis.com', 'via.placeholder.com', 'www.hostinger.com', 'img.clerk.com'],
   },
